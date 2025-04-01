a158abd72a42e0106d62ceec26dde348bf5af289
SanjayJohnson2 <sanjay.johnson@hotmail.com>
2025-04-01 15:26:14 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test, ExaModelsPower, MadNLP, MadNLPGPU, KernelAbstractions, CUDA, PowerModels, Ipopt
+using Test, ExaModelsPower, MadNLP, MadNLPGPU, KernelAbstractions, CUDA, PowerModels, Ipopt, JuMP, ExaModels
 
 const CONFIGS = [
     (Float32, nothing),
@@ -18,19 +18,13 @@ if CUDA.has_cuda_gpu()
     )
 end
 
-numeric_tolerance(::Type{Float32}) = 1e-2
-numeric_tolerance(::Type{Float64}) = 1e-4
-
-
 function runtests()
     @testset "ExaModelsPower test" begin
 
-        #Test static opf
-        #data_pm = PowerModels.parse_file("data/pglib_opf_case3_lmbd.m")
-
-        data, dicts = ExaModelsPower.parse_ac_power_data("pglib_opf_case3_lmbd.m")
-
         for (T, backend) in CONFIGS
+            #Test static opf
+            data, dicts = ExaModelsPower.parse_ac_power_data("pglib_opf_case3_lmbd.m")
+
             #Polar tests
             m, v, c = eval(opf_model)("pglib_opf_case3_lmbd.m"; T=T, backend = backend)
             result = madnlp(m; print_level = MadNLP.ERROR)
@@ -39,8 +33,8 @@ function runtests()
             nlp_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol"=>result.options.tol, "print_level"=>0)
             result_pm = solve_opf("data/pglib_opf_case3_lmbd.m",ACPPowerModel, nlp_solver)
      
-            @testset "$(T), $(backend), polar" begin
-                @test result.status == MadNLP.SOLVE_SUCCEEDED
+            @testset "static, $(T), $(backend), polar" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
                 @test isapprox(result.objective, result_pm["objective"], rtol = result.options.tol*100)
                 for key in keys(dicts.gen)
                     @test isapprox(Array(solution(result, pg))[dicts.gen[key]], result_pm["solution"]["gen"][string(key)]["pg"], atol = result.options.tol*100)
@@ -72,8 +66,8 @@ function runtests()
             nlp_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol"=>result.options.tol, "print_level"=>0)
             result_pm = solve_opf("data/pglib_opf_case3_lmbd.m", ACRPowerModel, nlp_solver)
      
-            @testset "$(T), $(backend), rect" begin
-                @test result.status == MadNLP.SOLVE_SUCCEEDED
+            @testset "static, $(T), $(backend), rect" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
                 @test isapprox(result.objective, result_pm["objective"], rtol = result.options.tol*100)
                 for key in keys(dicts.gen)
                     @test isapprox(Array(solution(result, pg))[dicts.gen[key]], result_pm["solution"]["gen"][string(key)]["pg"], atol = result.options.tol*100)
@@ -95,6 +89,180 @@ function runtests()
                     @test isapprox(Array(solution(result, var))[5], result_pm["solution"]["branch"]["3"][string(st_var, "t")], atol = result.options.tol*100)
                     @test isapprox(Array(solution(result, var))[6], result_pm["solution"]["branch"]["1"][string(st_var, "t")], atol = result.options.tol*100)
                 end
+            end
+            #Test MP
+            #Curve = [1, .9, .8, .95, 1]
+            true_sol = 25384.366465
+
+            function example_func(d, srating)
+                return d + .2/srating*d^2
+            end
+
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", [1, .9, .8, .95, 1]; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), curve, polar" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #w function
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", [1, .9, .8, .95, 1], example_func; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), curve, polar, func" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", [1, .9, .8, .95, 1]; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), curve, rect" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #w function
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", [1, .9, .8, .95, 1], example_func; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), curve, rect" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #Pregenerated Pd and Qd
+            true_sol = 29049.351564
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", "data/case3_5split.Pd", "data/case3_5split.Qd"; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), pregen, polar" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #w function
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", "data/case3_5split.Pd", "data/case3_5split.Qd", example_func; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), pregen, polar, func" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", "data/case3_5split.Pd", "data/case3_5split.Qd"; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), pregen, rect" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #w function
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd.m", "data/case3_5split.Pd", "data/case3_5split.Qd", example_func; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP, $(T), $(backend), pregen, rect, func" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #Test MP w storage
+
+            #Curve = [1, .9, .8, .95, 1]
+            true_sol = 25358.827525
+
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", [1, .9, .8, .95, 1]; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), curve, polar" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", [1, .9, .8, .95, 1]; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), curve, rect" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #With complementarity constraint
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", [1, .9, .8, .95, 1]; T = T, backend = backend, storage_complementarity_constraint = true)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), curve, polar, complementarity" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", [1, .9, .8, .95, 1]; T = T, backend = backend, form = :rect, storage_complementarity_constraint = true)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), curve, rect, complementarity" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #With function
+            true_sol = 25354.331998
+
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", [1, .9, .8, .95, 1], example_func; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), curve, polar, func" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", [1, .9, .8, .95, 1], example_func; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), curve, rect, func" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #Pregenerated Pd and Qd
+            true_sol = 29023.69118
+
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", "data/case3_5split.Pd", "data/case3_5split.Qd"; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), pregen, polar" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", "data/case3_5split.Pd", "data/case3_5split.Qd"; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), pregen, rect" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #With complementarity
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", "data/case3_5split.Pd", "data/case3_5split.Qd"; T = T, backend = backend, storage_complementarity_constraint = true)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), pregen, polar, complementarity" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", "data/case3_5split.Pd", "data/case3_5split.Qd"; T = T, backend = backend, form = :rect, storage_complementarity_constraint = true)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), pregen, rect, complementarity" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+
+            #With function
+            true_sol = 29019.172473
+
+            #Polar
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", "data/case3_5split.Pd", "data/case3_5split.Qd", example_func; T = T, backend = backend)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), pregen, polar, func" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
+            end
+            #Rect
+            m, v, c = eval(mpopf_model)("pglib_opf_case3_lmbd_mod.m", "data/case3_5split.Pd", "data/case3_5split.Qd", example_func; T = T, backend = backend, form = :rect)
+            result = madnlp(m; print_level = MadNLP.ERROR)
+            @testset "MP w storage, $(T), $(backend), pregen, rect, func" begin
+                @test result.status == MadNLP.SOLVE_SUCCEEDED || result.status == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                @test isapprox(result.objective, true_sol, rtol = result.options.tol*100)
             end
         end
     end
